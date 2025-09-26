06b0c8f86f44a9d376f004a6b02a5344c62ea1ec
Thespartann <danilastefan69@yahoo.ro>
2025-09-26 08:56:49 +0530

    soong: Whitelist perl
    * Fixes perl is not allowed to be used and some other perl warnings
    See https://android.googlesource.com/platform/build/+/master/Changes.md#PATH_Tools for more information.
    
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>
    Change-Id: Ib04526656d2cbf0de288259375e67f99d6fe5774
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -112,6 +112,7 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
+	"perl":    Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
